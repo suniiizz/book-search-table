53c3911c2f822ed91a395ef189568901d4deab18
jh <pleasemrlostman@naver.com>
2023-10-23 19:38:18 +0900

    update: shadcn/ui 설치

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+const App = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  return (
+    <>
+      <Button>Button</Button>
+      <Calendar mode="single" selected={date} onSelect={setDate} className="rounded-md border w-fit" />
+    </>
+  );
+};
+
+export default App;
