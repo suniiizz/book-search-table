4991a4e91c78688d90e63e4ffd8391a3cd8787a2
정재훈 <70499531+pleasemrlostman@users.noreply.github.com>
2023-10-30 23:32:54 +0900

    [UPDATE]: strict mode 최상단으로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,11 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </React.StrictMode>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>,
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    ,
+  </React.StrictMode>,
 );
